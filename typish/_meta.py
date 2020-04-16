cd274ebaa6cda00c5ec96690c6a167ef4c085be7
Ramon <p8u7wAPC5Pg9HYkkCkzA>
2020-04-16 21:00:57 +0200

    Version bump.

--- typish/_meta.py
+++ typish/_meta.py
@@ -1,5 +1,5 @@
 __title__ = 'typish'
-__version__ = '1.5.3'
+__version__ = '1.6.0'
 __author__ = 'Ramon Hagenaars'
 __author_email__ = 'ramon.hagenaars@gmail.com'
 __description__ = 'Functionality for types'
